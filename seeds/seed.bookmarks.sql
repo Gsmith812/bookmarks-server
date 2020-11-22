0b12c3be1be017090dead9af9f64bef6e4bdbe5c
George Smith <georgesmith812@gmail.com>
2020-11-22 18:11:04 -0500

    added seed scripts for adding dummy data to database and removing

---
+++ seeds/seed.bookmarks.sql
@@ -0,0 +1,12 @@
+INSERT INTO bookmarks (title, url, rating, description)
+VALUES
+    ('Portfolio Page', 'http//www.georgesmithiv.com', '5', 'My portfolio page.'),
+    ('Google', 'https://www.google.com', '4', 'Google Search engine.'),
+    ('Thinkful', 'https://www.thinkful.com', '5', null),
+    ('BKMC', 'http://www.thebkmc.com', '5', 'The Brooklyn Millinery Company, hat maker'),
+    ('Delta', 'http://www.delta.com', '5', 'Book your travel here.'),
+    ('Youtube', 'http://www.youtube.com', '3', 'Video service'),
+    ('Netflix', 'http://www.netflix.com', '5', 'Video Streaming service for shows and movies.'),
+    ('PostgreSQL', 'http://www.postgresql.org/', '4', null),
+    ('Heroku', 'http://www.heroku.com', '5', 'Platform for deploying back-end and front-end applications.'),
+    ('Github', 'http://www.github.com', '5', 'Used to store code repositories on a web server.');
